e4a405d6a99a9b058973057126596055f538ae84
danlorb <danlorb@velvet-lab.net>
2026-02-16 15:26:49 +0100

    Remove deprecated PowerShell and Bash scripts for PowerShell and VSCode configuration, SSH key management, and security functions. Clean up related test files and installation scripts to streamline the codebase.

--- new/src/load.bash
+++
@@ -1,8 +0,0 @@
-# Only for bats tests loading
-export __DEVOXIO_LIB_DIR
-
-__DEVOXIO_LIB_DIR=$(dirname "${BASH_SOURCE[0]}")
-
-# Include devoxio library
-source "${__DEVOXIO_LIB_DIR}/devoxio.sh"
-
--- new/src/next/modules/pwsh.ps1
+++
@@ -1,9 +0,0 @@
-
-$exist = Get-PSRepository -Name "PSGallery" -ErrorAction SilentlyContinue
-if ($null -ne $exist) {
-    Unregister-PSRepository -Name PSGallery -ErrorAction SilentlyContinue
-}
-$exist = Get-PSRepository -Name "Artifactory" -ErrorAction SilentlyContinue
-if ($null -eq $exist) {
-    Register-PSRepository -Name Artifactory -SourceLocation https://packages.repo.dvint.de/artifactory/api/nuget/nuget-powershellgallery-proxy/ -InstallationPolicy Trusted -ErrorAction SilentlyContinue
-}
