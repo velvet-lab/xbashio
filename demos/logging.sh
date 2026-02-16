60867603f1686b93cb286be500398c94df06071c
danlorb <danlorb@velvet-lab.net>
2026-02-16 15:36:15 +0100

    feat: add logging demo script with various log levels and messages

---
+++ demos/logging.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env xbashio
+
+xbashio::log "This is a default log message."
+xbashio::log.blue "This is a blue log message."
+xbashio::log.green "This is a green log message."
+xbashio::log.yellow "This is a yellow log message."
+xbashio::log.red "This is a red log message."
+xbashio::log.magenta "This is a magenta log message."
+xbashio::log.cyan "This is a cyan log message."
+
+xbashio::log.level all
+xbashio::log.trace "This is a trace log message."
+xbashio::log.debug "This is a debug log message."
+xbashio::log.info "This is an info log message."
+xbashio::log.notice "This is a notice log message."
+xbashio::log.warning "This is a warning log message."
+xbashio::log.error "This is an error log message."
+xbashio::log.fatal "This is a fatal log message."
